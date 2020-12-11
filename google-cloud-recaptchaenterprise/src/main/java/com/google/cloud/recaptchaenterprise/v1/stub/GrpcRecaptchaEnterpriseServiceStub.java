2cda023159738b35c08936c010622875e50f4d0d
yoshi-automation <yoshi-automation@google.com>
2020-12-10 16:30:11 -0800

    chore: update gapic-generator-java to 0.0.9
    Committer: @miraleung
    PiperOrigin-RevId: 346372957
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Dec 8 11:37:30 2020 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 857f70daef9f4c38f042fb3dfb7b8423ae18fd19
    Source-Link: https://github.com/googleapis/googleapis/commit/857f70daef9f4c38f042fb3dfb7b8423ae18fd19

--- google-cloud-recaptchaenterprise/src/main/java/com/google/cloud/recaptchaenterprise/v1/stub/GrpcRecaptchaEnterpriseServiceStub.java
+++ google-cloud-recaptchaenterprise/src/main/java/com/google/cloud/recaptchaenterprise/v1/stub/GrpcRecaptchaEnterpriseServiceStub.java
@@ -156,12 +156,22 @@ public class GrpcRecaptchaEnterpriseServiceStub extends RecaptchaEnterpriseServi
         callableFactory);
   }
 
+  /**
+   * Constructs an instance of GrpcRecaptchaEnterpriseServiceStub, using the given settings. This is
+   * protected so that it is easy to make a subclass, but otherwise, the static factory methods
+   * should be preferred.
+   */
   protected GrpcRecaptchaEnterpriseServiceStub(
       RecaptchaEnterpriseServiceStubSettings settings, ClientContext clientContext)
       throws IOException {
     this(settings, clientContext, new GrpcRecaptchaEnterpriseServiceCallableFactory());
   }
 
+  /**
+   * Constructs an instance of GrpcRecaptchaEnterpriseServiceStub, using the given settings. This is
+   * protected so that it is easy to make a subclass, but otherwise, the static factory methods
+   * should be preferred.
+   */
   protected GrpcRecaptchaEnterpriseServiceStub(
       RecaptchaEnterpriseServiceStubSettings settings,
       ClientContext clientContext,
