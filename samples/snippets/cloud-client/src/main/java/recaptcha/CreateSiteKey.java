8e404024b462d3b5f8f611aae7f003b058f5e8c4
SitaLakshmi <sita1996@gmail.com>
2021-08-03 18:23:54 +0530

    fix: changed key type. Added copyright.

--- samples/snippets/cloud-client/src/main/java/recaptcha/CreateSiteKey.java
+++ samples/snippets/cloud-client/src/main/java/recaptcha/CreateSiteKey.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package recaptcha;
 
 // [START recaptcha_enterprise_create_site_key]
@@ -7,7 +23,6 @@ import com.google.recaptchaenterprise.v1.CreateKeyRequest;
 import com.google.recaptchaenterprise.v1.Key;
 import com.google.recaptchaenterprise.v1.ProjectName;
 import com.google.recaptchaenterprise.v1.WebKeySettings;
-import com.google.recaptchaenterprise.v1.WebKeySettings.ChallengeSecurityPreference;
 import com.google.recaptchaenterprise.v1.WebKeySettings.IntegrationType;
 import java.io.IOException;
 
@@ -26,7 +41,7 @@ public class CreateSiteKey {
    * Create reCAPTCHA Site key which binds a domain name to a unique key.
    *
    * @param projectID : GCloud Project ID.
-   * @param domainName: Specify the domain name in which the reCAPTCHA should be activated.
+   * @param domainName : Specify the domain name in which the reCAPTCHA should be activated.
    */
   public static void createSiteKey(String projectID, String domainName) throws IOException {
     // Initialize client that will be used to send requests. This client only needs to be created
@@ -37,25 +52,24 @@ public class CreateSiteKey {
 
       // Set the type of reCAPTCHA to be displayed.
       // For different types, see: https://cloud.google.com/recaptcha-enterprise/docs/keys
-      Key checkboxKey = Key.newBuilder()
+      Key scoreKey = Key.newBuilder()
           .setDisplayName("any_descriptive_name_for_the_key")
           .setWebSettings(WebKeySettings.newBuilder()
               .addAllowedDomains(domainName)
               .setAllowAmpTraffic(false)
-              .setIntegrationType(IntegrationType.CHECKBOX)
-              .setChallengeSecurityPreference(ChallengeSecurityPreference.BALANCE).build())
+              .setIntegrationType(IntegrationType.SCORE).build())
           .build();
 
       CreateKeyRequest createKeyRequest = CreateKeyRequest.newBuilder()
           .setParent(ProjectName.of(projectID).toString())
-          .setKey(checkboxKey)
+          .setKey(scoreKey)
           .build();
 
       // Get the name of the created reCAPTCHA site key.
       Key response = client.createKey(createKeyRequest);
       String keyName = response.getName();
-      String recaptchaSiteKeyID = keyName.substring(keyName.lastIndexOf("/")+1);
-      System.out.println("reCAPTCHA Site key created successfully. ID: " + recaptchaSiteKeyID);
+      String recaptchaSiteKey = keyName.substring(keyName.lastIndexOf("/") + 1);
+      System.out.println("reCAPTCHA Site key created successfully. Site Key: " + recaptchaSiteKey);
     }
   }
 }
